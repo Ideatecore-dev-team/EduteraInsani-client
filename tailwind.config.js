53e64b8a8b30dc412e33adce4ffdb124e8740a2a
nhsltn <nhsltndev@gmail.com>
2024-05-27 03:15:32 +0700

    Initiate Package, Hero Component done 80%

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        xs: { max: "639px" },
+      },
+      colors: {
+        primary: "#0B4068",
+        secondary: "#A88646",
+        "neutral-1": "#212121",
+        "neutral-2": "#74757E",
+        background: "#FBF9F7",
+      },
+      width: {
+        "base-content": "1152px",
+      },
+    },
+  },
+  plugins: [],
+};
