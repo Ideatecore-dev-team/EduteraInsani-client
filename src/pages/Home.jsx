6b9eed60f100f264cb7d1e363d2d2db6b5428d1c
nhsltn <nhsltndev@gmail.com>
2024-05-29 21:37:11 +0700

    Details Section Done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Hero from "../components/Hero";
 import WhyEdutera from "../components/WhyEdutera";
+import Digital from "../components/Digital";
+import OurProgram from "../components/OurProgram";
 
 function Home() {
   return (
     <>
       <Hero />
       <WhyEdutera />
+      <Digital />
+      <OurProgram />
     </>
   );
 }
