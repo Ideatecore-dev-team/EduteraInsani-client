e1327627ab489a9c0b8ad27d83e268011a89bea5
WeissCurry <Maulanasykari@gmail.com>
2024-06-12 15:24:21 +0700

    try to solved minor bug

--- src/components/Buttons/ButtonCtg.jsx
+++ src/components/Buttons/ButtonCtg.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ButtonCtg.css";
 
-function ButtonCtg({ caption, buttonFn}) {
+function ButtonCtg({ caption, buttonFn, active}) {
   return (
     <button
       className={"btn-ctg py-3 px-4 items-center gap-2 text-base w-width-3"}
