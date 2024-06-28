e5c15e262bb607f4888f411a20775b8a1d59c955
WeissCurry <Maulanasykari@gmail.com>
2024-06-28 08:50:28 +0700

    FIX : AboutUs Page, Vision Section, Responsive

--- src/components/Vision.jsx
+++ src/components/Vision.jsx
@@ -1,9 +1,9 @@
 function Vision() {
   return (
-    <div className="vision-section flex pb-24 items-center justify-center self-stretch bg-background">
-      <div className="vision-container flex flex-col items-center justify-center gap-12">
-        <h2 className=" text-primary text-4xl  font-semibold">Visi Kami</h2>
-        <div className="vision-desc bg-white rounded-2xl p-6 justify-center items-center w-width-8">
+    <div className="vision-section flex xs:w-width-sm pb-12 lg:pb-24 xs:flex-col xs:gap-gap10 items-center lg:justify-center self-stretch bg-background">
+      <div className="vision-container xs:w-mobile-6 flex flex-col items-center lg:justify-center gap-6 lg:gap-12">
+        <h2 className=" text-primary text-2xl lg:text-4xl  font-semibold">Visi Kami</h2>
+        <div className="vision-desc bg-white rounded-2xl p-4 lg:p-6 justify-center items-center xs:self-stretch lg:w-width-8">
           <p className=" text-base text-neutral-2 font-normal text-center">
             Mewujudkan generasi Indonesia yang unggul secara keagamaan,
             berdasarkan Pancasila, dan ditandai dengan kepemimpinan, inovasi,
