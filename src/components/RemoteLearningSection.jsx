9756940507f85719b5bad2fbc4e823f342c73fc3
WeissCurry <Maulanasykari@gmail.com>
2024-06-29 15:18:45 +0700

    FIX : Curriculum page, RemoteLearningSection, responsive h-[399px]

--- src/components/RemoteLearningSection.jsx
+++ src/components/RemoteLearningSection.jsx
@@ -48,7 +48,7 @@ function RemoteLearning() {
     cardContent.map((card, index) => (
       <div
         key={index}
-        className="card-why flex lg:flex-row flex-col bg-white rounded-2xl w-mobile-6 lg:w-width-9 lg:p-6 p-4 items-start gap-4"
+        className="card-why flex lg:flex-row flex-col bg-white rounded-2xl w-mobile-6 xs:h-[399px] lg:w-width-9 lg:p-6 p-4 items-start gap-4"
       >
         <img src={card.img} className="size-20" alt="icon" />
         <div className="flex-col gap-6 ">
