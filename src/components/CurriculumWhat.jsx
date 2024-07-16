2d0e2d66331148c6ab7516ffb9b6a0e8a8317cca
WeissCurry <Maulanasykari@gmail.com>
2024-07-16 16:11:24 +0700

    FIX : Curriculum, images, curriculum-logo.png

--- src/components/CurriculumWhat.jsx
+++ src/components/CurriculumWhat.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "/images/curriculum-logo-fix.png";
+import Logo from "/images/curriculum-logo.png";
 import "./CurriculumWhat.css";
 
 function CurriculumWhat() {
@@ -7,7 +7,7 @@ function CurriculumWhat() {
     <div className="what-curriculum-section flex py-12 lg:py-24 items-center justify-center self-stretch bg-background mx-auto">
       <div className="what-curriculum-container lg:mt-0 flex flex-wrap gap-3 lg:flex-nowrap lg:gap-6 p-6 items-center w-mobile-6 lg:w-base-content mt-12 bg-white rounded-2xl">
         <div className="curriculum-img ml-2.5 flex p-6 justify-center items-center bg-background rounded-2xl">
-          <img src={Logo} alt="Kurikulum Merdeka" />
+          <img src={Logo} alt="Kurikulum Merdeka" className="size-[226px] lg:size-[270px]"/>
         </div>
         <div className="curriculum-desc flex flex-col items-start gap-4">
           <h2 className="text-primary text-2xl lg:text-4xl font-semibold">
